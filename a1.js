1bbd2f021d8f2a18752b6b250e413703488ceb0a
AMAN <amanshn2001@gmail.com>
2024-12-04 13:29:15 +0530

    added portfolio and sevices sections

--- a1.js
+++ a1.js
@@ -1,14 +1,15 @@
-myFun();
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents= document.getElementsByClassName("tab-contents");
 
-var myFun = function(){
-    console.log("first");
-};
-
-myFun();
- function myFun(){
-    console.log("second");
- }
-
- myFun();
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
 
 
